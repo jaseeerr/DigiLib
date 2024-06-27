12e26b815d12f3952e4dba041a952d7a05c2ce98
jaseeerr <jaseerhasbulla01@gmail.com>
2024-06-28 02:56:24 +0400

    finalFixes

--- client/src/pages/user/Login.jsx
+++ client/src/pages/user/Login.jsx
@@ -51,12 +51,12 @@ function LoginPage() {
     },[])
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-white px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen flex items-center justify-center  px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
             <div>
-                <span className='flex justify-center'>
+                {/* <span className='flex justify-center'>
                     <img src={logo} alt="" width='150px' />
-                </span>
+                </span> */}
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-[#F37123]">
                     Login 
                 </h2>
